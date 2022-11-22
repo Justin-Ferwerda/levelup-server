08136de5936e8adde7601541f656fddcfc4a7649
Justin Ferwerda <justin.ferwerda@gmail.com>
2022-11-21 19:30:47 -0600

    created update functions for games and events

--- levelupapi/views/event.py
+++ levelupapi/views/event.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from levelupapi.models import Event
+from levelupapi.models import Event, Game, Gamer
 
 
 class EventView(ViewSet):
@@ -32,6 +32,42 @@ class EventView(ViewSet):
             events = events.filter(game=game)
         serializer = EventSerializer(events, many=True)
         return Response(serializer.data)
+    
+    def create(self, request):
+        """Handle POST operations
+
+        Returns
+            Response -- JSON serialized event instance
+        """
+        game = Game.objects.get(pk=request.data["game"])
+        organizer = Gamer.objects.get(uid=request.data["organizer"])
+
+        event = Event.objects.create(
+            description=request.data["description"],
+            date=request.data["date"],
+            time=request.data["time"],
+            game=game,
+            organizer=organizer
+        )
+        serializer = EventSerializer(event)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        """Handle PUT requests for an event
+
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+
+        event = Event.objects.get(pk=pk)
+        event.description = request.data["description"]
+        event.date = request.data["date"]
+        event.time = request.data["time"]
+
+        event.game = Game.objects.get(pk=request.data["game"])
+        event.save()
+
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
       
 class EventSerializer(serializers.ModelSerializer):
     """JSON serializer for game types
